ba2ef050ee4dead7e931f23595fa5010fdd8800f
denakof <denakofahi_21@yahoo.com>
2021-04-27 00:07:45 +0300

    add

--- js/app.js
+++ js/app.js
@@ -7,14 +7,14 @@
 
  let Q1=prompt("Do you think i love sushi?").toLowerCase();
  switch (Q1) {
-     case ("yes") || ("y"):
+     case "yes":
+        case "y":
          console.log("it is correct")
-         document.write("it is correct")
          alert("its correct")
          break;
- case ("no" || "n"):
+ case "no":
+ case"n":
 console.log("false, i love sushii")
-document.write("false, i love sushii")
 alert("false i love sushii")
 break;
 
@@ -24,14 +24,14 @@ break;
 
  let Q2=prompt("do you think i can cook?").toLowerCase();
 switch (Q2) {
-    case ("yes" ||"y"):
+    case "yes":
+        case "y":
         console.log("wrong, i don't know how to cook")
-        document.write("wrong, i don't know how to cook")
         alert("wrong, i don't know how to cook")
         break;
-case ("no"|| "n"):
+case "no":
+     case"n":
     console.log("you know me")
-    document.write("you know me")
     alert("you know me")
     default:
         break;
@@ -40,42 +40,42 @@ case ("no"|| "n"):
 
 let Q3=prompt("Do you think i love pets?").toLowerCase();
 switch (Q3) {
-    case ("yes"||"y"):
+    case "yes":
+        case"y":
         console.log("correct, i adore pets")
-        document.write("correct, i adore pets")
         alert("correct, i adore pets")
         break;
-case ("no"|| "n"):
+case "no":
+     case "n":
 
 console.log("noo, i love them");
-document.write("noo, i love them");
 alert("No!, i love them")
         break;}
 
         let Q4=prompt("Does dena love kick-boxing?").toLowerCase();
         switch (Q4) {
-            case ("yes"|| "y"):
+            case "yes": 
+            case"y":
                 console.log("correct, i love kick-boxing")
-                document.write("correct, i love kick-boxing")
                 alert("correct, i love kick-boxing")
                 break;
-                case("no"|| "n"):
+                case "no": 
+                case"n":
                 console.log("wrong. it is one of my fav sports");
-                document.write("wrong. it is one of my fav sports");
                 alert("wrong. it is one of my fav sports")
                 break}
 
 
                 let Q5=prompt("Does dena know how to swim?").toLowerCase();
                 switch (Q5) {
-                    case ("yes"|| "y"):
+                    case"yes":
+                    case "y":
                         console.log("yes i know how to swim")
-                document.write("yes i know how to swim")
                 alert("yes i know how to swim")
                         break;
-                        case("no"|| "n"):
+                        case"no":
+                        case "n":
                         console.log("no i know how to swim");
-                        document.write("no i know how to swim");
                         alert("no i know how to swim")
                     default:
                         break;  }
